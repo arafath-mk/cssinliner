4c7485d8fd3fa8ab102251a441280226ca873ed1
Arafath M <arafath.mk@gmail.com>
2022-07-09 16:45:07 +0530

    match 2

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,8 @@ pub struct Settings {
 fn main() {
     // Settings.
     const CONFIG_FILE: &str = "cssinliner.json";
-    let settings_result = get_settings(CONFIG_FILE);
     let settings: Settings;
-    match settings_result {
+    match get_settings(CONFIG_FILE) {
         None => {
             eprintln!("Could not read the configuration file {}", CONFIG_FILE);
             return;
